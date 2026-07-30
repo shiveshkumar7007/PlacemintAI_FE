2e4f98a8e35a6951727849e8e779cbfbfa9626f1
shiveshkumar7007 <shiveshkumarid@gmail.com>
2026-07-30 13:44:43 +0530

    minor changes in resume analyser

--- src/services/resumeService.js
+++ src/services/resumeService.js
@@ -1,7 +1,5 @@
 import api from "./api";
 
-// Upload Resume
-
 export const uploadResume = async (file) => {
   const formData = new FormData();
 
@@ -15,25 +13,11 @@ export const uploadResume = async (file) => {
 
   return response.data;
 };
-
-// Analyze Resume
-
-export const analyzeResume = async (resumeId) => {
-  const response = await api.get(`/resume/analyze/${resumeId}`);
-
-  return response.data;
-};
-
-// Get Resume History
-
 export const getResumeHistory = async () => {
   const response = await api.get("/resume/history");
 
   return response.data;
 };
-
-// Get Latest Resume
-
 export const getLatestResume = async () => {
   const response = await api.get("/resume/latest");
 
